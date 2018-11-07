f859a26f3bc98c254cc3484f71818bed09c8dbf4
taniguchi <bayakau.tka@gmail.com>
2018-11-07 22:50:31 +0900

    Contentfulの

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,7 +1,14 @@
+const contentful = require("contentful");
+const config = require("./.contentful.json");
+
+const client = contentful.createClient({
+  space: config.CTF_SPACE_ID,
+  accessToken: config.CTF_CDA_ACCESS_TOKEN
+});
 const pkg = require('./package')
 
 module.exports = {
-  mode: 'universal',
+  mode: "universal",
 
   /*
   ** Headers of the page
@@ -9,37 +16,32 @@ module.exports = {
   head: {
     title: pkg.name,
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: pkg.description }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   /*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
+  loading: { color: "#fff" },
 
   /*
   ** Global CSS
   */
-  css: [
-  ],
+  css: [],
 
   /*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
+  plugins: [],
 
   /*
   ** Nuxt.js modules
   */
-  modules: [
-  ],
+  modules: [],
 
   /*
   ** Build configuration
@@ -48,8 +50,29 @@ module.exports = {
     /*
     ** You can extend webpack config here
     */
-    extend(config, ctx) {
-      
-    }
+    extend(config, ctx) {}
+  },
+  env: {
+    CTF_SPACE_ID: config.CTF_SPACE_ID,
+    CTF_CDA_ACCESS_TOKEN: config.CTF_CDA_ACCESS_TOKEN,
+    CTF_PERSON_ID: config.CTF_PERSON_ID,
+    CTF_BLOG_ID: config.CTF_BLOG_ID
   }
-}
+  ,
+  generate: {
+    routes() {
+      return Promise.all([
+        client.getEntries({
+          content_type: config.CTF_BLOG_ID,
+        }),
+        // client.getEntries({
+        //   content_type: config.CTF_CATEGORY_ID,
+        // }),
+      ])
+        .then(([posts, categories]) => [
+          ...posts.items.map(post => `articles/${post.fields.id}`),
+          // ...categories.items.map(category => `articles/category/${category.fields.slug}`),
+        ]);
+    },
+  }
+};
